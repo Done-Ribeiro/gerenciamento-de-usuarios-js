7b941cc1b406b683389a1c7b6b47bf9975d707de
Done Ribeiro <www.done_@hotmail.com>
2022-02-06 23:08:56 -0300

    delete users in localStorage

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -206,22 +206,9 @@ class UserController {
 
   }
 
-  getUsersStorage() {
-
-    let users = [];
-
-    // verifica se tem usuarios cadastrados
-    if (localStorage.getItem("users")) {
-      users = JSON.parse(localStorage.getItem("users"));
-    }
-
-    return users;
-
-  }
-
   selectAll() {
 
-    let users = this.getUsersStorage();
+    let users = User.getUsersStorage();
 
     users.forEach(dataUser => {
 
@@ -279,6 +266,11 @@ class UserController {
 
     tr.querySelector(".btn-delete").addEventListener("click", e => {
       if (confirm("Deseja realmente excluir?")) {
+
+        let user = new User();
+        user.loadFromJSON(JSON.parse(tr.dataset.user));
+        user.remove();
+
         tr.remove();
         this.updateCount();
       }
